2d9950eb11527734081ca82e595d0b1b58755e2f
UltimateProdigy <ayobamiakinola84@gmail.com>
2023-09-26 11:27:59 +0100

    Few Changes to Projects

--- src/Pages/Projects/Projectcard.jsx
+++ src/Pages/Projects/Projectcard.jsx
@@ -1,18 +1,23 @@
 import React from "react";
 import "./Projects.css";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
 
-const Projectcard = () => {
-    return (
-        <div className="projectcard">
-            <h1>INSURNCE</h1>
-            <h3>This is sample project description random things are here in description This is sample project lorem ipsum generator for dummy content</h3>
-            <h3>Tech Stack: HTML, CSS, JAVASCRIPT</h3>
-            <div className="preview">
-                <h3>Live Preview</h3>
-                <h3>View Code</h3>
-            </div>
-        </div>
-    )
-}
+const Projectcard = ({ name, description, stack, preview, code }) => {
+  return (
+    <div className="projectcard">
+      <h1>{name}</h1>
+      <h3>{description}</h3>
+      <h3>Tech Stack: {stack}</h3>
+      <div className="preview">
+        <Link to={{ pathname: `${preview}` }} target="_blank">
+          <h3>Live Preview</h3>
+        </Link>
+        <Link to={{ pathname: `${code}` }} target="_blank">
+          <h3>View Code</h3>
+        </Link>
+      </div>
+    </div>
+  );
+};
 
 export default Projectcard;
--- src/Pages/Projects/Projects.jsx
+++ src/Pages/Projects/Projects.jsx
@@ -1,30 +1,24 @@
 import React from "react";
 import "./Projects.css";
-import Navbar from "../../Component/Navbar/Navbar";
 import Projectcard from "./Projectcard";
 
 const Projects = () => {
-    return (
-        <div>
-            
-            <div className="projectdiv">
-            <div className="project">
-                <h1>Projects</h1>
-                <h3>Things i've built so far</h3>
-            </div>
-            <div className="all-projects">
-                <Projectcard />
-                <Projectcard />
-                <Projectcard />
-                <Projectcard />
-                <Projectcard />
-                <Projectcard />
-            </div>
-            </div>
-            
+  return (
+    <div>
+      <div className="projectdiv">
+        <div className="project">
+          <h1>Projects</h1>
+          <h3>Things i've built so far</h3>
         </div>
-
-    )
-}
+        <div className="all-projects">
+          <Projectcard name="INSURNCE" description="Developed an insurance landing page with interactive elements." stack="HTML, CSS, JAVASCRIPT" preview="https://insurnce.vercel.app/" code="https://github.com/UltimateProdigy/INSURNCE"/>
+          <Projectcard name="WORKOUT" description="Developed a high-intensity workout exercise plan web app to burn calories." stack="REACT, HTML, CSS, JAVASCRIPT" preview="https://workout-wine.vercel.app/" code="https://github.com/UltimateProdigy/WorkOut" />
+          <Projectcard name="WATCH" description="Created a movie app with a side menu." stack="REACT, HTML, CSS, JAVASCRIPT" preview="https://watch-zeta.vercel.app/" code="https://github.com/UltimateProdigy/Watch" />
+          <Projectcard name="GETLINKED" description="My Submission for a pre-Hackaton Competition" stack="REACT, HTML, CSS, JAVASCRIPT" preview="https://getlinked-weld.vercel.app/" code="https://github.com/UltimateProdigy/getlinked" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Projects;
